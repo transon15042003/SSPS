8099a692ce9e2478b8d15061d9c6038e87b2c68e
Trần Hoàng Sơn <transon15042003@gmail.com>
2024-12-01 22:52:42 +0700

    Update print report

--- client/src/components/print_report/FilterBar.js
+++ client/src/components/print_report/FilterBar.js
@@ -1,122 +1,129 @@
 import { useState } from "react";
 
 function FilterBar({ minMonth, minYear, typeState, monthState, yearState }) {
-  const [isMonth, setIsMonth] = useState(true);
+    const [isMonth, setIsMonth] = useState(true);
 
-  const range = (start, end) => {
-    return Array.from({ length: end - start }, (_, index) => end - 1 - index);
-  };
+    const range = (start, end) => {
+        return Array.from(
+            { length: end - start },
+            (_, index) => end - 1 - index
+        );
+    };
 
-  const [month, setMonth] = useState(range(1, 12 + 1));
-  const [setSelectedType] = typeState;
-  const [setSelectedMonth] = monthState;
-  const [setSelectedYear] = yearState;
+    const [month, setMonth] = useState(range(1, 12 + 1));
+    const [selectedType, setSelectedType] = typeState;
+    const [selectedMonth, setSelectedMonth] = monthState;
+    const [selectedYear, setSelectedYear] = yearState;
 
-  const today = new Date();
-  const currentMonth = today.getMonth() + 1;
-  const currentYear = today.getFullYear();
+    const today = new Date();
+    const currentMonth = today.getMonth() + 1;
+    const currentYear = today.getFullYear();
 
-  const yearOptionForMonthlyReport = range(minYear, currentYear + 1);
-  const yearOptionForAnnuallyReport = range(minYear, currentYear);
+    const yearOptionForMonthlyReport = range(minYear, currentYear + 1);
+    const yearOptionForAnnuallyReport = range(minYear, currentYear);
 
-  return (
-    <div className="row">
-      <label
-        htmlFor="start-date"
-        className="col-4 col-md-3 col-lg-auto col-form-label"
-      >
-        Chọn loại báo cáo
-      </label>
-      <div className="col-8 col-md-9 col-lg mb-2">
-        <select
-          className="form-select"
-          id="type"
-          name="type"
-          onChange={(event) => {
-            setIsMonth(event.target.value === "monthly");
-            setSelectedType(event.target.value);
-          }}
-        >
-          <option value="monthly" selected>
-            Theo tháng
-          </option>
-          <option value="annually">Theo năm</option>
-        </select>
-      </div>
-      <label
-        htmlFor="start-date"
-        className="col-4 col-md-3 col-lg-auto col-form-label"
-      >
-        Chọn năm
-      </label>
-      <div className="col-8 col-md-9 col-lg mb-2">
-        <select
-          className="form-select"
-          id="year"
-          name="year"
-          onChange={(event) => {
-            let year = event.target.value;
-            if (event.target.value !== "all") {
-              year = parseInt(year, 10);
-              if (year === currentYear) setMonth(range(1, currentMonth));
-              else if (year === minYear) setMonth(range(minMonth, 12 + 1));
-              else setMonth(range(1, 12 + 1));
-            } else {
-              setMonth(range(1, 12 + 1));
-            }
-            setSelectedYear(year);
-            if (document.getElementById("month") === "all")
-              setSelectedMonth("all");
-          }}
-        >
-          <option value="all" selected>
-            Tất cả
-          </option>
-          {isMonth
-            ? yearOptionForMonthlyReport.map((year) => (
-                <option key={year} value={year}>
-                  {year}
-                </option>
-              ))
-            : yearOptionForAnnuallyReport.map((year) => (
-                <option key={year} value={year}>
-                  {year}
-                </option>
-              ))}
-        </select>
-      </div>
-      {isMonth && (
-        <>
-          <label
-            htmlFor="end-date"
-            className="col-4 col-md-3 col-lg-auto col-form-label"
-          >
-            Chọn tháng
-          </label>
-          <div className="col-8 col-md-9 col-lg mb-2">
-            <select
-              className="form-select"
-              id="month"
-              name="month"
-              onChange={(event) => {
-                const month = event.target.value;
-                setSelectedMonth(month === "all" ? month : Number(month));
-              }}
+    return (
+        <div className="row">
+            <label
+                htmlFor="start-date"
+                className="col-4 col-md-3 col-lg-auto col-form-label"
             >
-              <option value="all" selected>
-                Tất cả
-              </option>
-              {month.map((month) => (
-                <option key={month} value={month}>
-                  {month}
-                </option>
-              ))}
-            </select>
-          </div>
-        </>
-      )}
-    </div>
-  );
+                Chọn loại báo cáo
+            </label>
+            <div className="col-8 col-md-9 col-lg mb-2">
+                <select
+                    className="form-select"
+                    id="type"
+                    name="type"
+                    onChange={(event) => {
+                        setIsMonth(event.target.value === "monthly");
+                        setSelectedType(event.target.value);
+                    }}
+                >
+                    <option value="monthly" selected>
+                        Theo tháng
+                    </option>
+                    <option value="annually">Theo năm</option>
+                </select>
+            </div>
+            <label
+                htmlFor="start-date"
+                className="col-4 col-md-3 col-lg-auto col-form-label"
+            >
+                Chọn năm
+            </label>
+            <div className="col-8 col-md-9 col-lg mb-2">
+                <select
+                    className="form-select"
+                    id="year"
+                    name="year"
+                    onChange={(event) => {
+                        let year = event.target.value;
+                        if (event.target.value !== "all") {
+                            year = parseInt(year, 10);
+                            if (year === currentYear)
+                                setMonth(range(1, currentMonth));
+                            else if (year === minYear)
+                                setMonth(range(minMonth, 12 + 1));
+                            else setMonth(range(1, 12 + 1));
+                        } else {
+                            setMonth(range(1, 12 + 1));
+                        }
+                        setSelectedYear(year);
+                        if (document.getElementById("month") === "all")
+                            setSelectedMonth("all");
+                    }}
+                >
+                    <option value="all" selected>
+                        Tất cả
+                    </option>
+                    {isMonth
+                        ? yearOptionForMonthlyReport.map((year) => (
+                              <option key={year} value={year}>
+                                  {year}
+                              </option>
+                          ))
+                        : yearOptionForAnnuallyReport.map((year) => (
+                              <option key={year} value={year}>
+                                  {year}
+                              </option>
+                          ))}
+                </select>
+            </div>
+            {isMonth && (
+                <>
+                    <label
+                        htmlFor="end-date"
+                        className="col-4 col-md-3 col-lg-auto col-form-label"
+                    >
+                        Chọn tháng
+                    </label>
+                    <div className="col-8 col-md-9 col-lg mb-2">
+                        <select
+                            className="form-select"
+                            id="month"
+                            name="month"
+                            onChange={(event) => {
+                                const month = event.target.value;
+                                setSelectedMonth(
+                                    month === "all" ? month : Number(month)
+                                );
+                            }}
+                        >
+                            <option value="all" selected>
+                                Tất cả
+                            </option>
+                            {month.map((month) => (
+                                <option key={month} value={month}>
+                                    {month}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default FilterBar;
--- client/src/components/utils/Footer.js
+++ client/src/components/utils/Footer.js
@@ -8,51 +8,55 @@ import { FaLocationDot } from "react-icons/fa6";
 import { MdCall, MdEmail } from "react-icons/md";
 
 function Footer() {
-  // const { user } = useContext(UserContext);
-  // const [pageLinks, setPageLinks] = useState([]);
+    // const { user } = useContext(UserContext);
+    // const [pageLinks, setPageLinks] = useState([]);
 
-  // useEffect(() => {
-  //     if (user.isSPSO) {
-  //         setPageLinks(spsoLinks);
-  //     } else {
-  //         setPageLinks(customerLinks);
-  //     }
-  // }, [user]);
+    // useEffect(() => {
+    //     if (user.isSPSO) {
+    //         setPageLinks(spsoLinks);
+    //     } else {
+    //         setPageLinks(customerLinks);
+    //     }
+    // }, [user]);
 
-  return (
-    <footer className="bg-dark pt-2 text-white">
-      <div className="container mt-4 px-3 px-md-0">
-        <div className="row mt-3 justify-content-around">
-          <div className="col-3 col-md-4 col-lg-5 mb-3 text-center">
-            <h6 className="text-uppercase fw-bold mb-3">HCMUT - SSPS</h6>
-            <img
-              src={logo_BK_border}
-              alt="Logo Bách Khoa TPHCM"
-              width={80}
-              height={80}
-              draggable="false"
-              className="mx-auto d-block"
-            />
-          </div>
-          <div className="col-3 col-md-4 col-lg-5 mb-md-0 mb-3">
-            <h6 className="text-uppercase fw-bold mb-3">Liên hệ</h6>
-            <p>
-              <FaLocationDot /> 268 Lý Thường Kiệt, phường 14, quận 10, TP.HCM
-            </p>
-            <p>
-              <MdCall /> (028) 38 651 670 - (028) 38 647 256 (Ext: 5258, 5234)
-            </p>
-            <p>
-              <MdEmail /> ssps@hcmut.edu.vn
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="text-center fw-bold p-3" id="cp-write">
-        &copy; {new Date().getFullYear()} Copyright
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="bg-dark pt-2 text-white">
+            <div className="container mt-4 px-3 px-md-0">
+                <div className="row mt-3 justify-content-around">
+                    <div className="col-3 col-md-4 col-lg-5 mb-3 text-center">
+                        <h6 className="text-uppercase fw-bold mb-3">
+                            HCMUT - SSPS
+                        </h6>
+                        <img
+                            src={logo_BK_border}
+                            alt="Logo Bách Khoa TPHCM"
+                            width={80}
+                            height={80}
+                            draggable="false"
+                            className="mx-auto d-block"
+                        />
+                    </div>
+                    <div className="col-3 col-md-4 col-lg-5 mb-md-0 mb-3">
+                        <h6 className="text-uppercase fw-bold mb-3">Liên hệ</h6>
+                        <p>
+                            <FaLocationDot /> 268 Lý Thường Kiệt, phường 14,
+                            quận 10, TP.HCM
+                        </p>
+                        <p>
+                            <MdCall /> (012) 34 567 89 - (012) 34 567 89 (Ext:
+                            1234, 1234)
+                        </p>
+                        <p>
+                            <MdEmail /> ssps@hcmut.edu.vn
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center fw-bold p-3" id="cp-write">
+                &copy; {new Date().getFullYear()} Copyright
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
