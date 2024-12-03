e9811905a4ba31b5bb88aff092770e3140e3f06d
Trần Sơn <transon15042003@gmail.com>
2024-12-03 10:22:31 +0700

    Fix UI

--- client/src/components/file_upload/FileUpload.js
+++ client/src/components/file_upload/FileUpload.js
@@ -6,105 +6,100 @@ import FileCards from "./FileCard";
 import UploadArea from "./UploadArea";
 
 function FileUpload({ id }) {
-    const bg = {
-        backgroundImage: `url(${fileupload_bg})`,
-        height: "auto",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        maxHeight: "80vh",
-    };
+  const bg = {
+    backgroundImage: `url(${fileupload_bg})`,
+    height: "auto",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    maxHeight: "80vh",
+  };
 
-    const [length, setLength] = useState(0);
-    const [files, setFiles] = useState([]);
+  const [length, setLength] = useState(0);
+  const [files, setFiles] = useState([]);
 
-    useEffect(() => {
-        const storedFiles = localStorage.getItem("files");
-        if (storedFiles) {
-            setFiles(JSON.parse(storedFiles));
-            setLength(JSON.parse(storedFiles).length);
-        }
-    }, [length]);
+  useEffect(() => {
+    const storedFiles = localStorage.getItem("files");
+    if (storedFiles) {
+      setFiles(JSON.parse(storedFiles));
+      setLength(JSON.parse(storedFiles).length);
+    }
+  }, [length]);
 
-    return (
-        <div
-            className="position-relative mx-auto shadow-lg"
-            style={bg}
-            id="file-upload"
-        >
-            <ProgressiveImage src={fileupload_bg}>
-                {(src, loading) => (
-                    <img
-                        className={`image${loading ? " loading" : " loaded"}`}
-                        src={src}
-                        alt="File Upload Background"
-                        style={{
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "cover",
-                            visibility: "hidden",
-                        }}
-                        draggable="false"
-                    />
-                )}
-            </ProgressiveImage>
-            <div
-                className="container-fluid position-absolute top-50 start-50 translate-middle rounded-4 shadow-lg"
-                style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.9)",
-                    width: "85%",
-                    height: "85%",
-                    padding: "2rem",
-                }}
-            >
-                <div className="row h-100">
-                    <div className="col-12">
-                        <div className="d-flex justify-content-center align-items-center mb-4">
-                            <div className="text-center">
-                                <h2 className="display-6 fw-bold mb-2">
-                                    Tải tài liệu
-                                </h2>
-                                <p className="text-muted fs-5">
-                                    Tải tài liệu bạn muốn in
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="row h-75">
-                            <div
-                                className={`${
-                                    files.length === 0 ? "col-12" : "col-7"
-                                } h-100 d-flex align-items-center justify-content-center`}
-                            >
-                                <UploadArea setLength={setLength} />
-                            </div>
-
-                            {files.length !== 0 && (
-                                <div className="col-5 h-100">
-                                    <div className="card h-100 border-0 bg-light">
-                                        <div className="d-flex flex-column card-body">
-                                            <h5 className="card-title fw-bold mb-3">
-                                                Tài liệu đã tải lên
-                                            </h5>
-                                            <div
-                                                className="d-flex flex-column justify-content-between overflow-auto"
-                                                style={{
-                                                    maxHeight:
-                                                        "calc(100% - 40px)",
-                                                    flexGrow: 1,
-                                                }}
-                                            >
-                                                <FileCards files={files} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div
+      className="position-relative mx-auto shadow-lg min-vh-100"
+      style={bg}
+      id="file-upload"
+    >
+      <ProgressiveImage src={fileupload_bg}>
+        {(src, loading) => (
+          <img
+            className={`image${loading ? " loading" : " loaded"}`}
+            src={src}
+            alt="File Upload Background"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              visibility: "hidden",
+            }}
+            draggable="false"
+          />
+        )}
+      </ProgressiveImage>
+      <div
+        className="container-fluid position-absolute top-50 start-50 translate-middle rounded-4 shadow-lg"
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.9)",
+          width: "85%",
+          height: "85%",
+          padding: "2rem",
+        }}
+      >
+        <div className="row h-100">
+          <div className="col-12">
+            <div className="d-flex justify-content-center align-items-center mb-4">
+              <div className="text-center">
+                <h2 className="display-6 fw-bold mb-2">Tải tài liệu</h2>
+                <p className="text-muted fs-5">Tải tài liệu bạn muốn in</p>
+              </div>
             </div>
+
+            <div className="row h-75">
+              <div
+                className={`${
+                  files.length === 0 ? "col-12" : "col-7"
+                } h-100 d-flex align-items-center justify-content-center`}
+              >
+                <UploadArea setLength={setLength} />
+              </div>
+
+              {files.length !== 0 && (
+                <div className="col-5 h-100">
+                  <div className="card h-100 border-0 bg-light">
+                    <div className="d-flex flex-column card-body">
+                      <h5 className="card-title fw-bold mb-3">
+                        Tài liệu đã tải lên
+                      </h5>
+                      <div
+                        className="d-flex flex-column justify-content-between overflow-auto"
+                        style={{
+                          maxHeight: "calc(100% - 40px)",
+                          flexGrow: 1,
+                        }}
+                      >
+                        <FileCards files={files} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default FileUpload;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,48 +5,48 @@ import { UserContext } from "../context/UserContext";
 import homeBG from "../assets/img/pexels-printer.jpg";
 
 function Home() {
-    const { user } = useContext(UserContext);
-    const [cookies] = useCookies();
-    const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const [cookies] = useCookies();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!cookies.auth && localStorage.length > 0) {
-            localStorage.clear();
-            navigate("/");
-        }
-    }, [cookies, navigate]);
+  useEffect(() => {
+    if (!cookies.auth && localStorage.length > 0) {
+      localStorage.clear();
+      navigate("/");
+    }
+  }, [cookies, navigate]);
 
-    return (
-        <div
-            className="position-relative h-100"
-            style={{
-                width: "100%",
-                height: "auto",
-                background: `url(${homeBG})`,
-                backgroundSize: "cover",
-                backgroundPosition: "right",
-                color: "white",
-                backgroundRepeat: "no-repeat",
-            }}
-        >
-            <div className="container-fluid position-absolute top-0 h-100">
-                <div className="row h-100">
-                    <div className="col-12 col-xl-6 d-flex justify-content-center align-items-md-center">
-                        <p
-                            className="pt-5 pt-sm-0 text-center fw-semibold"
-                            style={{
-                                width: "70%",
-                                fontSize: "5vw",
-                                color: "#1488db",
-                            }}
-                        >
-                            SSPS HCMUT
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      className="position-relative"
+      style={{
+        width: "100%",
+        minHeight: "80vh",
+        background: `url(${homeBG})`,
+        backgroundSize: "cover",
+        backgroundPosition: "right",
+        color: "white",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="container-fluid position-absolute top-0 h-100">
+        <div className="row h-100">
+          <div className="col-12 col-xl-6 d-flex justify-content-center align-items-xl-center">
+            <p
+              className="pt-0 pt-sm-5 text-center fw-semibold"
+              style={{
+                width: "70%",
+                fontSize: "5vw",
+                color: "#1488db",
+              }}
+            >
+              SSPS HCMUT
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
